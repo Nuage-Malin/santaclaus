05108a5bc8fc9d8be54b6793b855b5091036c3bb
Arthur <arthur.jourdan@epitech.eu>
2023-10-15 10:00:35 +0200

    chore(scripts/unit_tests.sh env/local_unit_tests.env): remove santaclaus db even if tests are run from outside docker and adjust env var when tests are run outside docker

--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -16,8 +16,9 @@ function stop_docker() {
   	fi
 }
 
+docker exec -t maestro-mongo-1 mongosh -u $MONGO_USERNAME -p $MONGO_PASSWORD --eval "use santaclaus" --eval "db.dropDatabase()" ## remove database santaclaus to start on a blank canevas for tests
+
 if [ "$1" == "--docker" ] ; then
-	docker exec -t maestro-mongo-1 mongosh -u $MONGO_USERNAME -p $MONGO_PASSWORD --eval "use santaclaus" --eval "db.dropDatabase()" ## remove database santaclaus to start on a blank canevas for tests
 	docker compose --env-file ./env/unit_tests.env --profile launch down --volumes
 	trap "echo \"Stopping docker container...\"; stop_docker $1; sleep 3; exit" SIGINT
 	docker compose --env-file ./env/unit_tests.env --profile launch up --build
